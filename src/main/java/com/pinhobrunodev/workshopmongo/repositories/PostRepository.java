0ef101cb270863f4fb75430e941cdbe5a5eda8f9
pinhobrunodev <iaishowcs@gmail.com>
2021-12-11 17:07:40 -0300

    GET posts by Title  MongoDB Query Syntax

--- src/main/java/com/pinhobrunodev/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/pinhobrunodev/workshopmongo/repositories/PostRepository.java
@@ -1,13 +1,18 @@
 package com.pinhobrunodev.workshopmongo.repositories;
 
 import com.pinhobrunodev.workshopmongo.models.entities.Post;
-import com.pinhobrunodev.workshopmongo.models.entities.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 
 public interface PostRepository extends MongoRepository<Post,String> {
+
+
+    //Query methods with MongoDB Syntax
+    @Query("{<'title'>: { $regex: ?0, $options: '<i>' } }")
+    List<Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String title);
 }

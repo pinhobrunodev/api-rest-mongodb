fe7e0bf327d4a2f0ff2162a4e47b5873bf42a2b1
pinhobrunodev <iaishowcs@gmail.com>
2021-12-11 15:37:22 -0300

    Database Seeding

--- src/main/java/com/pinhobrunodev/workshopmongo/config/TestConfig.java
+++ src/main/java/com/pinhobrunodev/workshopmongo/config/TestConfig.java
@@ -1,6 +1,7 @@
 package com.pinhobrunodev.workshopmongo.config;
 
 import com.pinhobrunodev.workshopmongo.models.embedded.Author;
+import com.pinhobrunodev.workshopmongo.models.embedded.Comment;
 import com.pinhobrunodev.workshopmongo.models.entities.Post;
 import com.pinhobrunodev.workshopmongo.models.entities.User;
 import com.pinhobrunodev.workshopmongo.repositories.PostRepository;
@@ -27,12 +28,26 @@ public class TestConfig {
         userRepository.deleteAll();
         postRepository.deleteAll();
 
-
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
-        User bob = new User(null, "Bob Gray", "bob@gmail.com");
-        
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, Instant.parse("2021-02-13T11:15:01Z"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new Author(maria));
+        Post post2 = new Post(null, Instant.parse("2021-02-14T10:05:49Z"), "Bom dia", "Acordei feliz hoje!", new Author(maria));
+
+        Comment c1 = new Comment("Boa viagem mano!", Instant.parse("2021-02-13T14:30:01Z"), new Author(alex));
+        Comment c2 = new Comment("Aproveite", Instant.parse("2021-02-13T15:38:05Z"), new Author(bob));
+        Comment c3 = new Comment("Tenha um ótimo dia!", Instant.parse("2021-02-14T12:34:26Z"), new Author(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c3));
+
+        postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 
 }
--- src/main/java/com/pinhobrunodev/workshopmongo/models/entities/User.java
+++ src/main/java/com/pinhobrunodev/workshopmongo/models/entities/User.java
@@ -56,4 +56,8 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
 }
